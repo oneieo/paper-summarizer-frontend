353636f6f9b5022b5b1cc1477da612115bbc4953
oneieo <sun020716@gmail.com>
2025-05-01 21:35:58 +0900

    fix: layout.tsx 헤더 구조 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,16 +21,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={pretendard.variable}>
-      <header className="w-full h-[4.375rem] px-[20rem] bg-[#1F325C] flex justify-between items-center">
-        <Image
-          src="/images/논문한입title.png"
-          alt="logo"
-          width={120}
-          height={120}
-        />
-        <h1 className="text-[#fffef8] text-[1.25rem] font-bold">header</h1>
-      </header>
-      <body className={pretendard.className}>{children}</body>
+      <body className={pretendard.className}>
+        {" "}
+        <div className="w-full h-[4.375rem] px-[20rem] bg-[#1F325C] flex justify-between items-center">
+          <Image
+            src="/images/논문한입title.png"
+            alt="logo"
+            width={120}
+            height={120}
+          />
+          <h1 className="text-[#fffef8] text-[1.25rem] font-bold">header</h1>
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
