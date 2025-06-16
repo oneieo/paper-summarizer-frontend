ccc26e3229ff787d65667bc4aa2fa1a47ed101c8
oneieo <sun020716@gmail.com>
2025-06-16 06:24:47 +0000

    chore: AuthCallback 수정

--- src/app/(auth)/_components/AuthCallback.tsx
+++ src/app/(auth)/_components/AuthCallback.tsx
@@ -19,16 +19,13 @@ const AuthCallback = () => {
           throw new Error("인증 코드를 찾을 수 없습니다");
         }
 
-        const response = await fetch(
-          `https://paper-summarizer-frontend.vercel.app/api/auth/github/callback?code=${code}`,
-          {
-            method: "GET",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            credentials: "include",
-          }
-        );
+        const response = await fetch(`/api/auth/github/callback?code=${code}`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+        });
 
         console.log("백엔드 응답 상태:", response.status);
 
